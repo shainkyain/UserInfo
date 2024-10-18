d075209255afeab5228d625acc1d22efc9e6db5c
Shainky Jain <jainshainky123@gmail.com>
2024-10-19 04:00:19 +0530

    new Controller01

--- src/main/java/com/devil/Spring/controller/controller.java
+++ src/main/java/com/devil/Spring/controller/controller.java
@@ -1,9 +1,11 @@
 package com.devil.Spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
+    @GetMapping("/")
     public String hello(){
         return "Kar ra hai kam";
     }
