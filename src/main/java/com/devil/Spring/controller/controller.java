dbf7a8dd93cd2fcf2557e2629322a0fd072c8250
Shainky Jain <jainshainky123@gmail.com>
2024-10-19 03:57:15 +0530

    new Controller

---
+++ src/main/java/com/devil/Spring/controller/controller.java
@@ -0,0 +1,10 @@
+package com.devil.Spring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    public String hello(){
+        return "Kar ra hai kam";
+    }
+}
