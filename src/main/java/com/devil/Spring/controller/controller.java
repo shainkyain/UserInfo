ffc3d65c154a76ce22a07da551144fcc3c9151ab
Shainky Jain <jainshainky123@gmail.com>
2024-10-19 04:04:41 +0530

    new Controller01

--- src/main/java/com/devil/Spring/controller/controller.java
+++ src/main/java/com/devil/Spring/controller/controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String hello(){
         return "Kar ra hai kam";
     }
